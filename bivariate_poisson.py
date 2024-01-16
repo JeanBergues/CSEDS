8b44087f8949fba3c6d84f39a0040dbc4d1ce1ed
rme2002 <romnick2005@gmail.com>
2024-01-16 15:52:51 +0100

    Added BP log likelihood

---
+++ bivariate_poisson.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math
+
+def pdf_bp(x, y, lambda):
+# Function to calculate the pdf of a bivariate poisson dist
+    product_component = np.exp(-(sum(lambda))) * ((lambda[0]**x)/(math.factorial(x))) * ((lambda[1]**y)/(math.factorial(y)))
+
+    sum_component = 0
+    for k in range(min(x, y)):
+        sum_component += math.comb(x, k) * math.comb(y, k) * math.factorial(k) * (((lambda[3])/(lambda[1]*lambda[2]))**k)
+
+    return product_component * sum_component
+
+def ll_biv_poisson(X, Y, Lambda, T, N):
+# Function for calculating the log likelihood of bivariate poisson dist
+    ll = 0
+    for t in range(T):
+        sum_1 = 0
+        for i in range(N/2):
+            sum_1 += np.log(pdf_bp(X[t, i], Y[t, i], Lambda[i,:]))
+        ll += sum_1
+
+    return ll
