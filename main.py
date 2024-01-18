9f1fe826e08918df7d2e444726f73bb628f6ad17
JeanBergues <jandemandan@outlook.com>
2024-01-18 23:28:07 +0100

    Added ability to predict specific games

--- main.py
+++ main.py
@@ -27,39 +27,51 @@ def read_and_filter_data(country: str, columns: list[str]) -> pd.DataFrame:
     return pd.concat(dataframes)
 
 
-def train_neural_network(data: pd.DataFrame, y_name: str, no_hidden_layers: int, hidden_layer_size: int):
+def train_neural_network_classifier(data: pd.DataFrame, y_name: str, no_hidden_layers: int, hidden_layer_size: int):
     # TODO: tweak other parameters in regressor function
-    neural_network = nn.MLPRegressor(hidden_layer_sizes=(no_hidden_layers, hidden_layer_size))
+    neural_network = nn.MLPClassifier(hidden_layer_sizes=(no_hidden_layers, hidden_layer_size))
     neural_network.fit(data.drop(y_name, axis=1), data[y_name])
+    return neural_network
 
 
 def main() -> None:
     # Set-up variables concerning data
     country = 'nl'
-    selected_columns = ["Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR"]
+    selected_columns = ["Date", "HomeTeam", "AwayTeam", "FTR"]
 
     # Set-up variables concerning neural network
-    no_hidden_layers = 1
-    hidden_layer_size = 10
+    no_hidden_layers = 2
+    hidden_layer_size = 30
     
     # Read in the data
-    data = read_and_filter_data(country, selected_columns)
+    data = read_and_filter_data(country, selected_columns).dropna(axis=0)
     # print(data)
 
     # Prepare the data for training a neural network
     # Make dates an ordinal integer value
     #data["Date"].apply(lambda d: datetime.date.toordinal(d))
 
-    # Factorize team names
-    # _, mapping = pd.
-    data[["HomeTeam", "AwayTeam"]] = data[["HomeTeam", "AwayTeam"]].apply(lambda x: pd.factorize(x)[0])
-    print(data)
+    # Factorize variables
+    columns_to_factorize = ["HomeTeam", "AwayTeam", "FTR"]
+    mappings = {}
+    for column in columns_to_factorize:
+        new_column, hometeam_mapping = pd.factorize(data[column])
+        data[column] = new_column
+        mappings.update({column: hometeam_mapping})
 
-    # Factorize scores
+    data[columns_to_factorize] = data[columns_to_factorize].apply(lambda x: pd.factorize(x)[0])
+    print(mappings['HomeTeam'])
+    print(mappings['FTR'])
 
     # Train the neural network
-    # train_neural_network(data.drop('Date', axis=1), 'FTR', no_hidden_layers, hidden_layer_size)
+    class_nn = train_neural_network_classifier(data.drop('Date', axis=1), 'FTR', no_hidden_layers, hidden_layer_size)
 
+    # Predict one specific game
+    pred_hometeam = mappings['HomeTeam'].get_loc('Vitesse')
+    pred_awayteam = mappings['AwayTeam'].get_loc('Nijmegen')
+    input_vec = pd.DataFrame({'HomeTeam': pred_hometeam, 'AwayTeam': pred_awayteam}, index=[0])
+
+    print(class_nn.predict(input_vec))
 
 if __name__ == "__main__":
     main()
